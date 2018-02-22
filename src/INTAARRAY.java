04c7bf5ada46cc0e60510bf06f2b3ad3749bd081
Asus_laptop\Asus <Palmy_kiss8@hotmail.com>
2018-02-22 23:16:37 +0700

    ข้อ 1

---
+++ src/INTAARRAY.java
@@ -0,0 +1,20 @@
+public class INTAARRAY {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []a= {1,2,3,4,5,6,7,8,9,10};
+		for(int i=0;i<10;i++) {
+			System.out.print("  \n"+a[i]);
+			
+			}
+		for(int j=9;j>=0;j--) {
+			System.out.print("\n "+a[j]);
+			
+		}
+			
+		}
+		
+		
+	
+
+}
